9de91c6498ed5db9d1897d8e64bf73b95fc121e5
PRIYANSH01 <priyanshverma9@gmail.com>
2023-02-17 16:52:11 +0530

    minour change

--- routes/user.js
+++ routes/user.js
@@ -20,7 +20,7 @@ router.get(
 router.get(
   "/login",
   passport.authenticate("google", {
-    successRedirect: process.env.FRONTWND_URL,
+    successRedirect: process.env.FRONTEND_URL,
   })
 
   // (req, res, next) => {
